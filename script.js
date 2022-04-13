777962419e3e198b32fb3c4588df6e55abd3a496
Egor Nedelko <gojiramedia@gmail.com>
2022-04-13 18:18:52 +0300

    new branch

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-// const dropBtn = document.querySelector('.drop-btn')
-// const dropContent = document.querySelector('.dropdown-content')
-// const links = document.querySelectorAll('.dropdown-content a')
-// const selectedLink = document.querySelector('.selected-link')
+// ________DROPDOWNS_______________________________________________
+const dropBtn = document.querySelector('.drop-btn')
+const dropContent = document.querySelector('.dropdown-content')
+const links = document.querySelectorAll('.dropdown-content a')
+const selectedLink = document.querySelector('.selected-link')
 
-// dropBtn.addEventListener('click', () => {
-//    dropContent.classList.contains('active') ? dropContent.classList.remove('active') : dropContent.classList.add('active')
-// })
-
-// links.forEach(link => {
-//    link.addEventListener('click', () => {
-//       selectedLink.textContent = link.textContent
-//       dropContent.classList.remove('active')
-//    })
-// })
-
-// _______________________________________________________
-const nameElem = document.getElementById('nameElem')
-const inputElem = document.getElementById('entity-name')
-
-nameElem.addEventListener('click', () => {
-   if (inputElem.getAttribute('type') == 'hidden') {
-      nameElem.style.display = 'none'
-      inputElem.setAttribute('type', 'text')
-   }
+dropBtn.addEventListener('click', () => {
+   dropContent.classList.contains('active') ? dropContent.classList.remove('active') : dropContent.classList.add('active')
 })
 
-window.addEventListener('click', (e) => {
-   // console.log(e.target.classList)
-   if (e.target.classList.contains('input-container') && inputElem.getAttribute('type') == 'text') {
-      nameElem.textContent = inputElem.value
-      nameElem.style.display = 'block'
-      inputElem.setAttribute('type', 'hidden')
-   }
+links.forEach(link => {
+   link.addEventListener('click', () => {
+      selectedLink.textContent = link.textContent
+      dropContent.classList.remove('active')
+   })
 })
+
+// ________CLICKABLE INPUT_______________________________________________
+// const nameElem = document.getElementById('nameElem')
+// const inputElem = document.getElementById('entity-name')
+
+// nameElem.addEventListener('click', () => {
+//    if (inputElem.getAttribute('type') == 'hidden') {
+//       nameElem.style.display = 'none'
+//       inputElem.setAttribute('type', 'text')
+//    }
+// })
+
+// window.addEventListener('click', (e) => {
+//    if (e.target.classList.contains('input-container') && inputElem.getAttribute('type') == 'text') {
+//       nameElem.textContent = inputElem.value
+//       nameElem.style.display = 'block'
+//       inputElem.setAttribute('type', 'hidden')
+//    }
+// })
